1072f65e7ed57fb698b02330f0fc90768c53384d
Vinicius0422 <guedescosta123@hotmail.com>
2024-02-21 20:56:46 -0300

    feat: crating sign up page

--- src/pages/signUp/index.jsx
+++ src/pages/signUp/index.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
+import logo from '../../assets/logo.png'
+
 
 export const SignUp = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
     return(
-        <div>SignUp</div>
+        <div className="login-container">
+            <div className="login-area">
+                <div className="login-logo">
+                    <img src={logo} alt="System logo"/>
+                </div>
+                <h1>Sign Up</h1>
+                <form action='submit'>
+                    <input type='text' placeholder='name' value={name} onChange={(e) => setName(e.target.value)}/>
+                    <input type='email' placeholder='email@email.com' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input type='password' placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+
+                    <button type='submit'>Sign Up</button>
+                </form>
+                <Link to='/'>Sign In</Link>
+            </div>
+        </div>
     )
 }
