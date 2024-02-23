700b210e97981080dc5f0d2eabfe73a19feec17e
Vinicius0422 <guedescosta123@hotmail.com>
2024-02-22 22:55:05 -0300

    feat: implementing signIn method

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -47,6 +47,36 @@ export const AuthProvider = ({ children }) => {
         })
     }
 
+    // Sign in a user
+    async function signIn(email, password){
+        setLoadingAuth(true);
+        await signInWithEmailAndPassword(auth, email, password)
+        .then( async (value) => {
+            let uid = value.user.uid;
+
+            const docRef = doc(db, "users", uid);
+            const docSnap = await getDoc(docRef);
+
+            let data = {
+                uid: uid,
+                name: docSnap.data().nome,
+                email: value.user.email,
+                avatarUrl: docSnap.data().avatarUrl
+            }
+
+            setUser(data)
+            storageUser(data)
+            setLoadingAuth(false)
+            toast.success("Welcome again!")
+            navigate("/dashboard")
+        })
+        .catch((error) => {
+            console.log(error);
+            setLoadingAuth(false);
+            toast.error("Something went wrong!");
+        })
+    }
+
     function storageUser(data){
         localStorage.setItem('@tickets-user', JSON.stringify(data));
     }
@@ -57,6 +87,7 @@ export const AuthProvider = ({ children }) => {
             signed: !!user,
             user,
             signUp,
+            signIn,
             loadingAuth
         }}>
             {children}
--- src/pages/signIn/index.jsx
+++ src/pages/signIn/index.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import logo from '../../assets/logo.png'
 import './signIn.css'
+import { AuthContext } from '../../contexts/auth'
 
 export const SignIn = () => {
+
+    const { signIn, loadingAuth } = useContext(AuthContext)
+
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    async function handleSignIn(e){
+        e.preventDefault();
+
+        if(email !== '' && password !== ''){
+            await signIn(email, password)
+        }
+    }
 
 
     return(
@@ -16,11 +28,13 @@ export const SignIn = () => {
                     <img src={logo} alt="System logo"/>
                 </div>
                 <h1>Sign In</h1>
-                <form action='submit'>
+                <form onSubmit={handleSignIn}>
                     <input type='email' placeholder='email@email.com' value={email} onChange={(e) => setEmail(e.target.value)}/>
                     <input type='password' placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} />
 
-                    <button type='submit'>Login</button>
+                    <button type='submit'>
+                        {loadingAuth ? 'Loading...' : 'Login'}
+                    </button>
                 </form>
                 <Link to='/register'>Sign Up</Link>
             </div>
